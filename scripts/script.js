0be35c2598d313f4031bc114070b25e6256f8323
Aditya <aditya4692@gmail.com>
2021-07-29 14:50:46 +0530

    adding create and delete options; saving to localstorage

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,11 @@ const itemCategory = document.getElementById('task-category');
 const toDoList = document.getElementById('to-do-list');
 const addBtn = document.getElementById('add-item-btn');
 const deleteBtns = document.querySelectorAll('.delete-item');
+const _todoList = "todolist";
 
+let _uniqueId = 0;
+
+displayList();
 addBtn.addEventListener('click', addItem);
 
 function addItem(){
@@ -12,68 +16,87 @@ function addItem(){
     let description = itemDescription.value;
 
     if(validateInput(category, description)){
-        let newListRow = createListRow(itemCategory.value, itemDescription.value);
-        toDoList.innerHTML += newListRow;
-        itemCategory.value = "";
-        itemDescription.value = "";
+        saveItem(category + "", description + "");   
+        displayList();
+        clearFields();
     }else{
+        //TODO use modal
         alert("enter values");
     }
-    // TODO: Add item to local storage
-    
-    /*let listItem = document.createElement("div");
-    listItem.classList.add("input-group");
-    listItem.classList.add("flex-nowrap");
-
-    
-
-    let listDescription = document.createElement("input");
-    listDescription.type = "text";
-    listDescription.classList.add("form-control");
-    listDescription.placeholder = "Description";
-
-    //let deleteContainer = document.createElement("span");
-    //deleteContainer.classList.add("input-group-append");
-
-    let deleteBtn = document.createElement("input");
-    deleteBtn.type = "button";
-    deleteBtn.classList.add("btn");
-    deleteBtn.classList.add("btn-danger");
-    deleteBtn.classList.add('input-group-append');
-    deleteBtn.value = "Delete";
-    deleteBtn.onclick = deleteItem();
-
-    //deleteContainer.appendChild(deleteBtn);
-    listItem.appendChild(listDescription);
-    listItem.appendChild(deleteBtn);
-    toDoList.appendChild(listItem); */
 }
+
+function clearFields(){
+    itemCategory.value = "";
+    itemDescription.value = "";
+}
+
+function getLocalStorageData(storageKey){
+    let dataStr = localStorage.getItem(storageKey);
+    let dataArr;
+
+    if(dataStr == null || dataStr == ""){
+        dataArr = [];
+    }else{
+        dataArr = JSON.parse(dataStr);
+    }
+    return dataArr;
+}
+
+function setLocalStorageData(key, data){
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+function saveItem(category, description){
+    let todoListDataArr = getLocalStorageData(_todoList);
+    let itemId = getUniqueId();
+
+    todoListDataArr.push({_id: itemId, cat: category, desc: description});
+    setLocalStorageData(_todoList, todoListDataArr);
+}
+
+function displayList(){
+    let todoListData = getLocalStorageData(_todoList);
+
+    toDoList.innerHTML = todoListData.reduce((gridContent, row, index) =>{
+        return gridContent += createListRow(index, row.cat, row.desc);
+    }, "");
+}
+
 function validateInput(category, description){
     if(category!=="" || description!=""){
         return true;
     }
     return false;
 }
-function createListRow(category, description){
+function createListRow(index, category, description){
     let gridRow = 
     `<div class="row align-items-center pd-3">
+        <div class="col-1">
+            ${index + 1}
+        </div>
         <div class="col-2">
             <b>${category}</b>
         </div>
         <div class="col-6">
             ${description}
         </div>
-        <div class="col-2">
+        <div class="col-1">
             <input type="button" class="btn btn-success" value="Mark Done">
         </div>
-        <div class="col-2">
-            <input type="button" onclick="deleteItem(this)" class="btn btn-danger delete-item" value="Delete">
+        <div class="col-1">
+            <input type="button" onclick="deleteItem(${index})" class="btn btn-danger delete-item" value="Delete">
         </div>
     </div>`
     return gridRow;
 }
 
-function deleteItem(btn){
-    //TODO: delete the item
-    btn.parentElement.parentElement.style.display = "hidden";
+function deleteItem(index){
+    let todoListData = getLocalStorageData(_todoList);
+    todoListData.splice(index, 1);
+    setLocalStorageData(_todoList, todoListData);
+    displayList();
+}
+
+function getUniqueId(){ //inutil right now
+    return _uniqueId + 1;
 }
